7d6467792154a3e45214649b84becaff07d2860f
Manav Codaty <111678991+manavcodaty@users.noreply.github.com>
2024-03-13 05:50:23 +0000

    Add meal planning functionality

--- mealplanningbot.py
+++ mealplanningbot.py
@@ -1,2 +1,31 @@
 #Meal planning bot 
+import pandas as pd
+
+
+def add_recipe():
+    meal = input("Enter the name of the meal: ")
+    pd.tocsv(meal, index=False)
+    
+
+def main_menu():
+    print("Hello! Welcome to the meal planning bot!")
+    print("Please select an option from the following:")
+    print("1. Add a meal")
+    print("2. View meals")
+    print("3. Plan a meal")
+    print("4. View meal plan")
+    print("5. Exit")
+    print("Enter the number of the option you would like to select: ")
+    option = input()
+    
+    if option == "1":
+        add_recipe()
+    elif option == "2":
+        view_recipes()
+    elif option == "3":
+        plan_meal()
+    elif option == "4":
+        view_meal_plan()
+    elif option == "5":
+        exit()
 
